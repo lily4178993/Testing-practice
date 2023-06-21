ecc19c8cebec310900289826382bdb61b9a129a1
lily4178993 <nellytelli@gmail.com>
2023-06-21 06:39:29 +0000

    Created 'multiply' method

--- js/calculator.js
+++ js/calculator.js
@@ -22,4 +22,12 @@ class calculator {
             throw new Error ('Enter a valid number');
         }
     }
+
+    multiply = (number1, number2) => {
+        if(typeof(number1) === 'number' && typeof(number2) === 'number' && number1 && number2){
+            return number1 * number2;
+        } else {
+            throw new Error ('Enter a valid number');
+        }
+    }
 }
