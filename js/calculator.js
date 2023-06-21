5d0de7d9d802766ebafe8cd135881d95445a4202
lily4178993 <nellytelli@gmail.com>
2023-06-21 06:37:57 +0000

    Created 'subtract' method

--- js/calculator.js
+++ js/calculator.js
@@ -6,4 +6,12 @@ class calculator {
             throw new Error ('Enter a valid number');
         }
     }
+
+    subtract = (number1, number2) => {
+        if(typeof(number1) === 'number' && typeof(number2) === 'number' && number1 && number2){
+            return number1 - number2;
+        } else {
+            throw new Error ('Enter a valid number');
+        }
+    }
 }
