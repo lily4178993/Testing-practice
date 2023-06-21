8fcca18e8fcd6babd5eaa6241ee7d7c3bd7c9423
lily4178993 <nellytelli@gmail.com>
2023-06-21 06:36:32 +0000

    Created 'add' method

--- js/calculator.js
+++ js/calculator.js
@@ -1,3 +1,9 @@
 class calculator {
-    
+    add = (number1, number2) => {
+        if(typeof(number1) === 'number' && typeof(number2) === 'number' && number1 && number2){
+            return number1 + number2;
+        } else {
+            throw new Error ('Enter a valid number');
+        }
+    }
 }
