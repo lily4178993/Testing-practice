183ef2f77ab8f393b3605de491155aa497dd5405
lily4178993 <nellytelli@gmail.com>
2023-06-21 05:44:16 +0000

    Replace toEqual with .toBe in expect statement

--- test/stringLength.test.js
+++ test/stringLength.test.js
@@ -4,5 +4,13 @@ test('Length of "test" to equal 4', () => {
 
     const string = 'test';
 
-    expect(stringLength(string)).toEqual(4);
+    expect(stringLength(string)).toBe(4);
+});
+
+test ('Length of "hello world" and "" to throw error', () => {
+    const string1 = 'hello world';
+    const string2 = '';
+
+    expect(() => stringLength(string1) && stringLength(string2))
+    .toThrow('String length out of range');
 });
